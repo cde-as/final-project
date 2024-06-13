aee1adf0406cd091729b33d1ef51fad0d0275698
Ina Ricci <ina_ricci@hotmail.com>
2024-06-12 18:00:35 -0700

    add POST method

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".frontend/public/index.{html,js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
